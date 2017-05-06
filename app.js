ba28bf1622051823f2fb335d3c3b3cfdf024124b
Nsikan <nsikane@live.co.uk>
2017-05-06 11:44:40 +0100

    Refactored app.js to isolate application logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-var moment = require('moment');
+const moment = require('moment');
 const path = require('path');
-const ResponseJSON = require(path.resolve('./routes/ResponseJSONCLass.js'));
 
+const timestampMaker = require(path.resolve('./app/timestampMaker.js')).timestampMaker;
 
 const port = 3000;
 var myapp = express();
@@ -16,7 +16,7 @@ myapp.get('/',function(req,res){
 });
 
 myapp.get('/:what',function(req,res){
-    res.json(genUnixAndNaturalDate(req.params.what));
+    res.json(timestampMaker(req.params.what));
 });
 
 
@@ -25,16 +25,3 @@ myapp.get('/:what',function(req,res){
 myapp.listen(port,function(){
     console.log(`Currently listening on port ${port}`);
 });
-
-function genUnixAndNaturalDate(input){
-    const dateFormat = "D MMM YYYY";
-    if(Number.isInteger(Number(input)) && moment.unix(Number(input)).isValid()){ //if the input can be converted to an integrer, use UTC conversion on it
-        let inputMoment = moment.unix(Number(input));
-        return new ResponseJSON(inputMoment.valueOf(),inputMoment.format(dateFormat));
-    } else if(moment.utc(input,dateFormat,true).isValid()) {
-        let inputMoment = moment.utc(input,dateFormat,true);
-        return new ResponseJSON(inputMoment.valueOf(),inputMoment.format(dateFormat));
-    } else {
-        return new ResponseJSON(null,null);
-    }
-   }
