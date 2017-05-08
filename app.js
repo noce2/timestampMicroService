7885b9c7fa97b7412e9465d181d8183d06663b8f
Nsikan <nsikane@live.co.uk>
2017-05-08 05:20:15 +0100

    Integrated ESLint & used it to tidy code

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const moment = require('moment');
 const path = require('path');
 
 const Timestamp = require(path.resolve('./app/timestamp.class.js')).Timestamp;
 
 const port = 3000;
-var myapp = express();
-
-
+let myapp = express();
 
 
 /* ROUTING */
-myapp.get('/',function(req,res){
-    res.send("Welcome to My Microservice")
+myapp.get('/', function(req, res) {
+    res.send('Welcome to My Microservice');
 });
 
-myapp.get('/:what',function(req,res){
+myapp.get('/:what', function(req, res) {
     res.json(Timestamp.timestampMaker(req.params.what));
 });
 
 
-/*START UP SERVER*/ 
+/* START UP SERVER*/
 
-myapp.listen(port,function(){
-    console.log(`Currently listening on port ${port}`);
+myapp.listen(port, function() {
+    // console.log(`Currently listening on port ${port}`);
 });
--- app/timestamp.class.js
+++ app/timestamp.class.js
@@ -1,24 +1,31 @@
 const moment = require('moment');
 
-function Timestamp (unix, natural){
-     this.unix = unix;
-     this.natural = natural; 
+/**
+ * Represents my desired Timestamp result from the service
+ * @unix is a unix time input
+ * @natural is a string input of text date to the 'D MMM YYYY' format
+ */
 
+function Timestamp(unix, natural) {
+     this.unix = unix;
+     this.natural = natural;
  }
 
- Timestamp.timestampMaker = function(input){
-    
-    const dateFormat = "D MMM YYYY";
-    if(Number.isInteger(Number(input)) && moment.unix(Number(input)).isValid()){ 
-        //if the input can be converted to an integrer, use UTC conversion on it
+ Timestamp.timestampMaker = function(input) {
+    const dateFormat = 'D MMM YYYY';
+    if(Number.isInteger(Number(input))
+     && moment.unix(Number(input)).isValid()) {
+        // if the input can be converted to an integrer, use UTC conversion
         let inputMoment = moment.unix(Number(input));
-        return new Timestamp(inputMoment.unix(),inputMoment.format(dateFormat));
-    } else if(moment.utc(input,dateFormat,true).isValid()) {
-        let inputMoment = moment.utc(input,dateFormat,true);
-        return new Timestamp(inputMoment.unix(),inputMoment.format(dateFormat));
+        return new Timestamp(inputMoment.unix(),
+         inputMoment.format(dateFormat));
+    } else if(moment.utc(input, dateFormat, true).isValid()) {
+        let inputMoment = moment.utc(input, dateFormat, true);
+        return new Timestamp(inputMoment.unix(),
+         inputMoment.format(dateFormat));
     } else {
-        return new Timestamp(null,null);
+        return new Timestamp(null, null);
     }
-   }
+   };
 
  module.exports.Timestamp = Timestamp;
